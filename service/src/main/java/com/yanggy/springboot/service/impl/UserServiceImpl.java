0dc985ffd7903ea73aa61d939e7b1e5f44d162ed
yangguiyun <yangguiyun@mobanker.com>
2017-09-28 13:14:10 +0800

    use mycat to replace mysql

--- service/src/main/java/com/yanggy/springboot/service/impl/UserServiceImpl.java
+++ service/src/main/java/com/yanggy/springboot/service/impl/UserServiceImpl.java
@@ -1,26 +1,14 @@
 package com.yanggy.springboot.service.impl;
 
-import com.yanggy.springboot.entity.User;
 import com.yanggy.springboot.mapper.UserMapper;
-import com.yanggy.springboot.jwt.JWTUser;
-import com.yanggy.springboot.jwt.JwtTokenUtil;
 import com.yanggy.springboot.service.UserService;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
-import java.util.Date;
 
 /**
  * Created by yangguiyun on 2017/9/26.
