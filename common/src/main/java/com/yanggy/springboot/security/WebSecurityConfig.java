a9f6d2493512e5448e0563c22ba8ecf2b366fba9
yangguiyun <yangguiyun@mobanker.com>
2017-10-11 17:41:26 +0800

    remove unused imports

--- common/src/main/java/com/yanggy/springboot/security/WebSecurityConfig.java
+++ common/src/main/java/com/yanggy/springboot/security/WebSecurityConfig.java
@@ -4,7 +4,6 @@ import com.yanggy.springboot.jwt.JwtAuthenticationTokenFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
